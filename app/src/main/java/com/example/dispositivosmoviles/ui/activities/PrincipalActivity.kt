ba997df62c5e3ced9b0cf5ee3a4bec7a4463860b
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-06 19:36:34 -0500

    Navigation Bar

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/PrincipalActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.ActivityMainBinding
 import com.example.dispositivosmoviles.databinding.ActivityPrincipalBinding
+import com.google.android.material.snackbar.Snackbar
 
 class PrincipalActivity : AppCompatActivity() {
 
@@ -28,19 +29,14 @@ class PrincipalActivity : AppCompatActivity() {
         super.onStart()
 
         var name : String = ""
-
-        //funcion let{}
-        //let -> descompone un objeto e itera similar a  "While"
-        //apply -> coger un boton y aplicar diferentes cosas
-        //with -> transformar
-        intent.extras.let {
+        /*intent.extras.let {
             name = it?.getString("var1")!!
-        }
+        }*/
 
-        Log.d("UCE","Hola ${name}")
+        //Log.d("UCE","Hola ${name}")
         binding.textName.text = "Bienvenido "+name.toString()
 
-        Log.d("UCE","Entrada a Start")
+        //Log.d("UCE","Entrada a Start")
 
         binding.botonUno.setOnClickListener {
             //iniciar el objeto intent
@@ -52,6 +48,44 @@ class PrincipalActivity : AppCompatActivity() {
             )
 
         }
+
+
+        binding.bottomNavigation.setOnItemSelectedListener() { item ->
+            when(item.itemId) {
+                R.id.inicio -> {
+                    var suma:Int = 0
+                    for(i in listOf(1,2,3)){
+                        suma += 1
+                    }
+
+                    // Respond to navigation item 1 click
+                    Snackbar.make(binding.textName,"La suma es {$suma}", Snackbar.LENGTH_LONG).show()
+                    true
+                }
+                R.id.favoritos -> {
+                    // Respond to navigation item 2 click
+                    binding.textName.text = "Entramos a Favoritos"
+
+                    var suma:Int = 0
+                    for(i in listOf(8,12,13)){
+                        suma += 1
+                    }
+
+                    // Respond to navigation item 1 click
+                    Snackbar.make(binding.textName,"La suma es {$suma}", Snackbar.LENGTH_LONG).show()
+
+                    true
+                }
+                R.id.apis -> {
+                    // Respond to navigation item 2 click
+                    binding.textName.text = "Entramos a Apis"
+                    Snackbar.make(binding.textName,"Entramos a Apis", Snackbar.LENGTH_LONG).show()
+                    true
+                }
+                else -> false
+            }
+        }
+
 
     }
 
