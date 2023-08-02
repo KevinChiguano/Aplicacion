7824eeeb5cdf954890b2c49e23c958f4e754e11a
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-08-01 19:48:44 -0500

    View Model

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/NotificationActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/NotificationActivity.kt
@@ -1,17 +1,23 @@
 package com.example.dispositivosmoviles.ui.activities
 
 import android.annotation.SuppressLint
+import android.app.AlarmManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.NotificationManagerCompat.from
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.ActivityNotificationBinding
+import com.example.dispositivosmoviles.ui.utilities.BroadcasterNotifications
+import java.util.Calendar
 
 class NotificationActivity : AppCompatActivity() {
 
@@ -26,6 +32,48 @@ class NotificationActivity : AppCompatActivity() {
             //createNotificationChannel()
             sendNotification()
         }
+
+        binding.btnNotificationProgramada.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val hora = binding.timePicker.hour
+            val minutes = binding.timePicker.minute
+
+            Toast.makeText(
+                this,
+                "La notificacion se activara a las: $hora:$minutes",
+                Toast.LENGTH_LONG
+            ).show()
+
+            calendar.set(Calendar.HOUR, hora)
+            calendar.set(Calendar.MINUTE, minutes)
+            calendar.set(Calendar.SECOND, 0)
+
+            sendNotificationTimePicker(calendar.timeInMillis)
+        }
+    }
+    @SuppressLint("ScheduleExactAlarm")
+    private fun sendNotificationTimePicker(time: Long) {
+
+        val myIntent = Intent(
+            applicationContext,
+            BroadcasterNotifications::class.java
+        )
+
+        val myPendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            0,
+            myIntent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        alarmManager.setExact(
+            AlarmManager.RTC_WAKEUP,
+            time,
+            myPendingIntent
+        )
+
     }
 
 
@@ -60,7 +108,8 @@ class NotificationActivity : AppCompatActivity() {
         noti.setSmallIcon(R.drawable.home_48px)
         noti.setPriority(NotificationCompat.PRIORITY_DEFAULT)
         noti.setStyle(NotificationCompat.BigTextStyle()
-            .bigText("Esta es una notificacion para recordar que estamos trabajando en Android"))
+            .bigText("Esta es una notificacion para recordar que estamos trabajando en Android")
+        )
 
         with(from(this)){
             notify(1,noti.build())
