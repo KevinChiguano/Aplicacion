efa264331b39c184d39f866ddf97b0a3dd937d3a
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-08-07 19:28:15 -0500

    Shimmer layout

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/BiometricActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/BiometricActivity.kt
@@ -4,18 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
+import androidx.activity.viewModels
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
 import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.ActivityBiometricBinding
+import com.example.dispositivosmoviles.ui.viewmodels.BiometricViewModel
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 
 class BiometricActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBiometricBinding
+    private val biometricViewModel by viewModels<BiometricViewModel> ()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +30,27 @@ class BiometricActivity : AppCompatActivity() {
 
         binding.btnAutentication.setOnClickListener {
             autenticateBiometric()
+
+            lifecycleScope.launch {
+                biometricViewModel.charginData()
+            }
         }
+
+        biometricViewModel.isloading.observe(this){ isLoading ->
+            if(isLoading){
+                binding.lytMain.visibility = View.GONE
+                binding.lytMainCopia.visibility = View.VISIBLE
+            }else{
+                binding.lytMain.visibility = View.VISIBLE
+                binding.lytMainCopia.visibility = View.GONE
+            }
+
+        }
+
+        lifecycleScope.launch {
+            biometricViewModel.charginData()
+        }
+
     }
 
     private fun autenticateBiometric() {
